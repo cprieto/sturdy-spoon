5cefdd6291766303e8f4044a1a7992ae7234e31d
Cristian Prieto <me@cprieto.com>
2022-08-01 17:16:55 +0200

    Start with the tracker component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+    import Tracker from './components/Tracker.vue';
 </script>
 
 <template>
@@ -45,47 +46,7 @@
         </div>
 
         <!-- Start instrument track -->
-        <div class="box level has-background-light">
-            <div class="level-item level-left">
-                <p>Instrument</p>
-            </div>
-            <div class="buttons are-small level-item m-0">
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-                <a href="#" class="button track-item"></a>
-            </div>
-            <div class="level-item level-right buttons">
-                <button class="button" title="Single">
-                    <span class="icon is-small">
-                        <i class="fa-solid fa-ear-listen"></i>
-                    </span>
-                </button>
-                
-                <button class="button" title="Mute">
-                    <span class="icon is-small">
-                        <i class="fa-solid fa-volume-xmark"></i>
-                    </span>
-                </button>
-                <button class="button" title="Remove instrument">
-                    <span class="icon is-small">
-                        <i class="fa-solid fa-trash-can"></i>
-                    </span>
-                </button>
-            </div>
-        </div>
+        <Tracker />
 
         <div class="box level has-background-light">
             <div class="level-item level-left">
@@ -252,7 +213,3 @@
     </div>
   </section>
 </template>
-
-<style lang="scss">
-@import "index.scss";
-</style>
---
+++ src/components/Tracker.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="box level has-background-light">
+    <div class="level-item level-left">
+      <p>Instrument</p>
+    </div>
+    <div class="buttons are-small level-item m-0">
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+      <a href="#" class="button track-item"></a>
+    </div>
+    <div class="level-item level-right buttons">
+      <button class="button" title="Single">
+        <span class="icon is-small">
+          <i class="fa-solid fa-ear-listen"></i>
+        </span>
+      </button>
+
+      <button class="button" title="Mute">
+        <span class="icon is-small">
+          <i class="fa-solid fa-volume-xmark"></i>
+        </span>
+      </button>
+      <button class="button" title="Remove instrument">
+        <span class="icon is-small">
+          <i class="fa-solid fa-trash-can"></i>
+        </span>
+      </button>
+    </div>
+  </div>
+</template>
