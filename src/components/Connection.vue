bfbc31d66947c922298e7cc8a1cf32a4fb7d2b74
Cristian Prieto <me@cprieto.com>
2022-08-04 19:42:25 +0200

    We have a session component

--- src/components/Connection.vue
+++ src/components/Connection.vue
@@ -1,8 +1,15 @@
+<script setup lang="ts">
+import { useSessionStore } from '@/stores/Session.js';
+
+const store = useSessionStore();
+
+</script>
+
 <template>
   <div class="level block">
     <div class="field is-grouped">
       <p class="control is-expanded">
-        <input type="text" class="input" placeholder="Session id" />
+        <input type="text" class="input" placeholder="Session id" v-model="store.sessionId" />
       </p>
       <p class="control">
         <a href="#" class="button is-info">Join</a>
@@ -11,7 +18,8 @@
 
     <div class="field">
       <p class="control">
-        <input type="text" class="input" placeholder="username" />
+        <input type="text" class="input" placeholder="username" 
+          v-model="store.nickname" :readonly="true" />
       </p>
     </div>
   </div>
