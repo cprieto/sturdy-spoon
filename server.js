a10b821c4f543475493ac0c46ce21b23d882486e
Cristian Prieto <me@cprieto.com>
2022-08-18 10:04:57 +0200

    FinProj: Ready to deploy to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import serveStatic from 'serve-static';
+import history from 'connect-history-api-fallback';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,6 +9,10 @@ const __dirname = path.dirname(__filename);
 const dist = path.join(__dirname, 'dist');
 
 const app = express();
+app.use(history());
 app.use(serveStatic(dist));
 
-app.listen(process.env.PORT || 3008);
+const server = app.listen(process.env.PORT || 3008, () => {
+    const port = server.address().port;
+    console.info(`App running at: http://localhost:${port}`);
+});
